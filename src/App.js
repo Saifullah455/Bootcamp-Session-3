334c20e542754e3fb10ee9e091406f2a0ba983a6
Saifullah455 <41240651+Saifullah455@users.noreply.github.com>
2020-06-22 15:02:39 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Biodata from './biodata';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Biodata name="SAIF ULLAH" class="BSSE section 'A'" cgpa="3.2"/>
     </div>
   );
 }
---
+++ src/biodata.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Biodata (props){
+   return(
+    <div>
+        <h2>STUDENT'S BIODATA</h2>
+        <div>Name: {props.name}</div>
+        <div>Class: {props.class}</div> 
+        <div>CGPA: {props.cgpa}</div>
+    </div>
+   );
+}
+
+export default Biodata
